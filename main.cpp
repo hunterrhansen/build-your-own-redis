0195612d492834190eddeb73d6eb704991b4ac4b
Hunter Hansen <hunterhansen50@gmail.com>
2025-03-10 08:49:29 -0600

    redis-clone begin tcp server

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "tcp-server.h"
 
 int main() {
     std::cout << "Hello, redis!" << std::endl;
+    
+    TCPServer server("fd", "address");
+
     return 0;
 }
--- tcp-server.h
+++ tcp-server.h
@@ -1,7 +1,10 @@
 #pragma once
 
+#include <string>
 class TCPServer {
     public:
-    
+        TCPServer(std::string fd, std::string address);
     private:
-}
+        std::string fd;
+        std::string address;
+};
